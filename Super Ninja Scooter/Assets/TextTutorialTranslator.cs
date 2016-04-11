e5f35679ba7bb8b326cb141cb8fb8747315feb24
Jacob Dubose <jdubose@utc4me.org>
2016-04-11 10:29:34 -0400

    cool beans
    cool memes

---
+++ Super Ninja Scooter/Assets/TextTutorialTranslator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextTutorialTranslator : MonoBehaviour {
+
+	[SerializeField] float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (0, 0, -moveSpeed * Time.deltaTime);
+	}
+}
